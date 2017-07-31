f55f83a6dd80b056f0a25c64c8077360f098ec0f
Jovica Aleksic <jovica.aleksic@xailabs.de>
2017-07-31 18:33:37 +0200

    tests: use babel-tape-runner and faucet

--- test/createCall.test.js
+++ test/createCall.test.js
@@ -1,40 +1,96 @@
 import test from 'tape';
 import Alt from 'alt';
+import ActionListeners from 'alt-utils/lib/ActionListeners';
 
-import {createCall} from '../src';
+import {createCall, createStore} from '../src';
 import {setAltInstance} from '../src/altInstance';
 
-test('createCall throws without a given name', function (t) {
-    setAltInstance(new Alt());
+function resetAlt() {
+    const alt = new Alt();
+    setAltInstance(alt);
+    return alt;
+}
+
+
+test('createCall throws without a given name', (t) => {
+    resetAlt();
     t.throws(() => createCall());
     t.end();
 });
-test('createCall does not throw with a given name', function (t) {
-    setAltInstance(new Alt());
+test('createCall does not throw with a given name', (t) => {
+    resetAlt();
     t.doesNotThrow(() => createCall('myCall'));
     t.end();
 });
 
-test('createCall: define() throws without a given object', function (t) {
-    setAltInstance(new Alt());
-    const call = createCall('someCall');
+test('createCall: define() throws without a given object', (t) => {
+    resetAlt();
+    const call = createCall('myCall');
     t.throws(() => call.define());
     t.end();
 });
 
-test('createCall: define() throws without a proper dataSource', function (t) {
-    setAltInstance(new Alt());
-    const call = createCall('someCall');
+test('createCall: define() throws without a proper dataSource', (t) => {
+    resetAlt();
+    const call = createCall('myCall');
     t.throws(() => call.define({}));
     t.throws(() => call.define({dataSource: {}}));
     t.end();
 });
 
-test('createCall: define() does not throw when given a proper dataSource ', function (t) {
-    setAltInstance(new Alt());
-    const call = createCall('someCall');
-    t.doesNotThrow(() => call.define({dataSource: {
-        remote: () => Promise.resolve()
-    }}));
+test('createCall: define() does not throw when given a proper dataSource ', (t) => {
+    resetAlt();
+    const call = createCall('myCall');
+    t.doesNotThrow(() => call.define({
+        dataSource: {
+            remote: () => Promise.resolve()
+        }
+    }));
+    t.end();
+});
+
+test('createCall: uses custom named actions', async (t) => {
+    const alt = resetAlt();
+
+    let remote = () => Promise.resolve();
+
+    const call = createCall('myCall', {defaultActions: ['foo', 'bar', 'baz']}).define(({actions}) => {
+        return {
+            dataSource: {
+                loading: actions.foo,
+                error: actions.bar,
+                success: actions.baz,
+                remote: () => remote()
+            }
+        };
+    });
+    
+    const store = createStore('MyStore', {calls: [call]});
+
+    const dispatched = {
+        loading: false,
+        success: false,
+        error: false
+    };
+
+    const listeners = new ActionListeners(alt);
+    listeners.addActionListener(call.actions.FOO, () => dispatched.loading = true);
+    listeners.addActionListener(call.actions.BAR, () => dispatched.error = true);
+    listeners.addActionListener(call.actions.BAZ, () => dispatched.success = true);
+    
+    await store.myCall();
+    t.equals(dispatched.loading, true, 'custom loading action dispatched');
+    t.equals(dispatched.success, true, 'custom success action dispatched');
+
+    remote = () => Promise.reject(new Error('nok'));
+    try {
+        await store.myCall();
+    }
+    catch (error) {
+        // do nothing
+        // expected error.
+    }
+
+    t.equals(dispatched.error, true, 'custom error action dispatched');
     t.end();
 });
--- test/createStore.test.js
+++ test/createStore.test.js
@@ -1,40 +1,129 @@
-import test from 'tape';
-import {createStore} from '../src';
-import {setAltInstance} from '../src';
+import test from 'tape'
 import Alt from 'alt';
+import ActionListeners from 'alt-utils/lib/ActionListeners';
 
-test('createStore: uses alt instance specified globally', function (t) {
+import {createStore, createCall, getSources, setAltInstance} from '../src';
+
+
+function resetAlt() {
     const alt = new Alt();
     setAltInstance(alt);
-    const store = createStore({displayName: 'MyStore'});
-    t.equals(store.alt, alt);
-    t.end();
-});
-test('createStore: uses specified alt instance', function (t) {
+    return alt;
+}
+
+
+test('createStore: uses alt instance specified globally', (t) => {
     const alt = new Alt();
-    const store = createStore({displayName: 'MyStore', alt: alt});
+    setAltInstance(alt);
+    const store = createStore('MyStore');
     t.equals(store.alt, alt);
     t.end();
 });
-test('createStore: throws when displayName is missing', function (t) {
-    function createBlank() {
-        return createStore();
-    }
-    t.throws(createBlank, /displayName is required/);
+test('createStore: uses specified alt instance', (t) => {
+    const alt = new Alt();
+    const store = createStore('MyStore', {alt: alt});
+    t.equals(store.alt, alt);
     t.end();
 });
-test('createStore: does not throw if displayName is specified', function (t) {
-    function createNamed() {
-        return createStore({displayName: 'MyStore'});
-    }
-    t.doesNotThrow(createNamed, /displayName is required/);
+test('createStore: throws when displayName is missing', (t) => {
+    resetAlt();
+    t.throws(() => createStore(), /displayName is required/);
     t.end();
 });
-test('createStore: uses specified displayName', function (t) {
-    setAltInstance(new Alt());
-    const store = createStore({displayName: 'MyStore'});
+test('createStore: does not throw if displayName is specified', (t) => {
+    resetAlt();
+    t.doesNotThrow(() => createStore('MyStore'), /displayName is required/);
+    t.end();
+});
+test('createStore: uses specified displayName', (t) => {
+    resetAlt();
+    const store = createStore('MyStore');
     t.equals(store.displayName, 'MyStore');
     t.end();
 });
 
+
+
+
+test('createStore: bound calls are exposed as store methods', (t) => {
+    resetAlt();
+    const call = createCall('myCall').define({
+        dataSource: {
+            remote: () => Promise.resolve()
+        }
+    });
+    const calls = [call];
+    const store = createStore('MyStore', {calls});
+    t.equals(typeof store.myCall, 'function');
+    t.end();
+});
+
+
+
+test('createStore: bound calls dispatch lifecycle actions', async (t) => {
+
+    const alt = resetAlt();
+    const goodCall = createCall('goodCall').define({
+        dataSource: {
+            remote: () => Promise.resolve('ok')
+        }
+    });
+    const badCall = createCall('badCall').define({
+        dataSource: {
+            remote: () => Promise.reject(new Error('nok'))
+        }
+    });
+    const store = createStore('MyStore', {
+        calls: [
+            goodCall,
+            badCall
+        ]
+    });
+
+    const dispatched = {
+        goodCall: {
+            loading: false,
+            error: false,
+            success: false
+        },
+        badCall: {
+            loading: false,
+            error: false,
+            success: false
+        }
+    };
+
+
+    const listeners = new ActionListeners(alt);
+
+    listeners.addActionListener(goodCall.actions.LOADING, () => dispatched.goodCall.loading = true);
+    listeners.addActionListener(goodCall.actions.ERROR, () => dispatched.goodCall.error = true);
+    listeners.addActionListener(goodCall.actions.SUCCESS, () => dispatched.goodCall.success = true);
+
+    listeners.addActionListener(badCall.actions.LOADING, () => dispatched.badCall.loading = true);
+    listeners.addActionListener(badCall.actions.ERROR, () => dispatched.badCall.error = true);
+    listeners.addActionListener(badCall.actions.SUCCESS, () => dispatched.badCall.success = true);
+
+    await store.goodCall();
+
+    try {
+        await store.badCall();
+    }
+    catch (error) {
+        // do nothing here.
+        // this is support to throw - we want to assert that the error action was dispatched.
+    }
+
+
+    t.equals(dispatched.goodCall.loading, true, 'lifecycle: loading action dispatched');
+    t.equals(dispatched.goodCall.error, false, 'lifecycle: error action not dispatched');
+    t.equals(dispatched.goodCall.success, true, 'lifecycle: success action dispatched');
+
+    t.equals(dispatched.badCall.loading, true, 'lifecycle: loading action dispatched');
+    t.equals(dispatched.badCall.error, true, 'lifecycle: error action not dispatched');
+    t.equals(dispatched.badCall.success, false, 'lifecycle: success action not dispatched');
+
+
+    t.end();
+});
 
