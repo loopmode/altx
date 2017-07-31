c596427f00ba1abf6f5fe4c0f601eaff6543fa74
Jovica Aleksic <jovica.aleksic@loopmode.de>
2017-07-31 07:43:16 +0200

    Use default alt instance

--- src/altInstance.js
+++ src/altInstance.js
@@ -1,11 +1,45 @@
+/**
+ * This is the alt instance that holds the state of the entire application.
+ * All stores and actions are created on this instance.
+ */
+
+import Alt from 'alt';
+
 let instance;
 
+setAltInstance(new Alt());
+
 export function setAltInstance(alt) {
-    instance = alt;
+    if (instance) teardown(instance);
+    setup(alt);
 }
 export function getAltInstance() {
-    if (!instance) {
-        throw new Error('No alt instance specified - did you forget to call setAlt(instance) initially?');
-    }
     return instance;
+}
+
+
+
+function setup(alt) {
+    instance = alt;
+    if (process.env.NODE_ENV !== 'production') {
+
+        // Debugging with chrome devtools
+        // @see https://github.com/goatslacker/alt-devtool
+        require('alt-utils/lib/chromeDebug')(instance);
+
+        // sometimes, chromeDebug just doesn't update until an action is dispatched.
+        // we use a dummy action to do just that
+        const refreshAction = instance.generateActions('__refresh__').__refresh__;
+        instance.handleMessage = (e) => {
+            if (e.data && e.data.type === 'ALT' && e.data.source === 'alt-devtools') {
+                refreshAction.defer();
+            }
+        };
+        window.addEventListener('message', instance.handleMessage);
+    }
+}
+function teardown(alt) {
+    if (process.env.NODE_ENV !== 'production') {
+        window.removeEventListener('message', instance.handleMessage);
+    }
 }
