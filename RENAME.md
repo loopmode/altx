58e487d39241a53175e327790f8930f83253173c
Jovica Aleksic <jovica.aleksic@loopmode.de>
2017-10-16 08:10:37 +0200

    1.1.2: update readme and docs, bump version

--- RENAME.md
+++
@@ -1,7 +0,0 @@
-# altx
-
-Flux variant based on [alt.js](https://github.com/goatslacker/alt)
-
-[![docs](https://doc.esdoc.org/github.com/loopmode/altx/badge.svg)](https://doc.esdoc.org/github.com/loopmode/altx/)
-
-NOTE: Depends on a `Promise` implementation being available. Make sure you polyfill in case you need to support old browsers.
--- src/createAction.js
+++ src/createAction.js
@@ -1,8 +1,14 @@
 import createActions from './createActions';
 
 /**
- * Creates a single simple (data-pass-through) action with a namespace.
- * Basically just like `alt.generateActions`, but within a given namespace instead of 'global'.
+ * Creates a single action with a namespace.
+ * Basically just like `altx.generateActions`, but with a signle required argument `name` and an optional second argument `namespace`.
+ *
+ * Note: The returned object is very much the same as with the plural version `createActions`: You get back an object that has
+ * a function by the name you gave as well as an constant for the action name.
+ *
+ * @example
+ * createAction('myAction', 'myNamespace'); // {name: 'myNamespace', myAction: function() {...}, MY_ACTION: 'myNamespace.myAction'}
  *
  * @param {string} name - The name to use for the action
  * @param {string} namespace - The namespace to use for the action
